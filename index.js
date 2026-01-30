7b05add927134f7b3a56a8dcf6e3b339ffefc6e9
ChiomaE <chioma.eriken@gmail.com>
2026-01-30 17:58:42 -0600

    create todo on button submit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+let listOfTodos = [];
+
+//Create new todo on button submit
+function submitTodo() {
+  let todoInput = document.getElementById("todoInput");
+  console.log(todoInput.value);
+  let newTodoItem = document.createElement("li");
+  newTodoItem.classList.add("todoItem");
+  newTodoItem.textContent = todoInput.value;
+  todoList.appendChild(newTodoItem);
+}
+
+let submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", submitTodo);
+
+let todoList = document.getElementById("todoList");
