3bb60e6d4d1b19e50e9e974940e246bd14ae4d64
ChiomaE <chioma.eriken@gmail.com>
2026-01-30 18:20:08 -0600

    delete todo on click

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-let listOfTodos = [];
+let firstTodo = true;
+
+function createEventTodoListener() {}
 
 //Create new todo on button submit
 function submitTodo() {
   let todoInput = document.getElementById("todoInput");
-  console.log(todoInput.value);
   let newTodoItem = document.createElement("li");
   newTodoItem.classList.add("todoItem");
   newTodoItem.textContent = todoInput.value;
+  newTodoItem.addEventListener("click", deleteTodo);
   todoList.appendChild(newTodoItem);
+}
+
+function deleteTodo() {
+  todoList.removeChild(event.target);
 }
 
 let submitBtn = document.getElementById("submitBtn");
