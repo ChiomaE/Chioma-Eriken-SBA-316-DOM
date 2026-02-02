f455e670e8087fa01b084b1ee98558f24e74f58b
ChiomaE <chioma.eriken@gmail.com>
2026-02-02 17:44:00 -0600

    update page layout, add window functions

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-let firstTodo = true;
+let todoInput;
+let newTodoItem;
+let userName = "";
 
-function createEventTodoListener() {}
+//Get user's name to display on start
+if (userName === "" || userName === null) {
+  userName = window.prompt("Enter your name below.");
+}
+let mainTitle = document.getElementById("mainContent");
+let titleDiv = document.createElement("h1");
+titleDiv.textContent = `${userName}'s Todo List`;
+console.log(titleDiv);
+mainTitle.appendChild(titleDiv);
 
 //Create new todo on button submit
 function submitTodo() {
-  let todoInput = document.getElementById("todoInput");
-  let newTodoItem = document.createElement("li");
+  todoInput = document.getElementById("todoInput");
+  newTodoItem = document.createElement("li");
   newTodoItem.classList.add("todoItem");
   newTodoItem.textContent = todoInput.value;
   newTodoItem.addEventListener("click", deleteTodo);
@@ -13,10 +23,12 @@ function submitTodo() {
 }
 
 function deleteTodo() {
-  todoList.removeChild(event.target);
+  if (window.confirm("Are you sure you want to delete this todo item?")) {
+    todoList.removeChild(event.target);
+  }
 }
 
-let submitBtn = document.getElementById("submitBtn");
+let submitBtn = document.querySelector("#submitBtn");
 submitBtn.addEventListener("click", submitTodo);
 
 let todoList = document.getElementById("todoList");
