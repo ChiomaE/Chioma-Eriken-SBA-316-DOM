856cd446bc8bb128461953d1206967f632540f1a
ChiomaE <chioma.eriken@gmail.com>
2026-02-02 18:35:02 -0600

    changed page styles, add form validation

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-let todoInput;
+// let todoInput;
+todoInput = document.getElementById("todoInput");
+const regexTest = /\d/;
 let newTodoItem;
 let userName = "";
 
 //Get user's name to display on start
-if (userName === "" || userName === null) {
+userName = window.prompt("Enter your name below.");
+while (userName === "" || userName === null) {
   userName = window.prompt("Enter your name below.");
 }
-let mainTitle = document.getElementById("mainContent");
+let mainTitle = document.getElementById("todoTitle");
 let titleDiv = document.createElement("h1");
 titleDiv.textContent = `${userName}'s Todo List`;
 console.log(titleDiv);
@@ -14,7 +17,18 @@ mainTitle.appendChild(titleDiv);
 
 //Create new todo on button submit
 function submitTodo() {
-  todoInput = document.getElementById("todoInput");
+  //Check if todo contains number. if so, show message, color the input box, then return
+  if (regexTest.test(todoInput.value)) {
+    todoInput.style.backgroundColor = "red";
+    alert(
+      "Todo items cannot contain numbers. Please enter a todo item without numbers.",
+    );
+    return;
+  } else if (todoInput.value === "") {
+    alert("Input cannot be empty. Please enter a valid todo item.");
+    return;
+  }
+  todoInput.style.backgroundColor = "white";
   newTodoItem = document.createElement("li");
   newTodoItem.classList.add("todoItem");
   newTodoItem.textContent = todoInput.value;
